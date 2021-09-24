0f04a2bb3aab271f88305c40ba12db0ea2db3825
Leithen <leithen113@gmail.com>
2021-09-24 08:22:39 -0600

    Added JS for form submission. Prevented default form behavior in browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form"); //assign variable to form element
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
+var createTaskHandler = function(event) {
+    event.preventDefault(); //removes default browser behavior
+
+    var listItemEl = document.createElement("li"); //create new li within ul
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler); //listen for form, submission
 
