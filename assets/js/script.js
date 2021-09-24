aa607dc46b99cfb97d6a29179d477020a1d67614
Leithen <leithen113@gmail.com>
2021-09-24 08:46:06 -0600

    Form now created list item with users input

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,18 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     event.preventDefault(); //removes default browser behavior
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     var listItemEl = document.createElement("li"); //create new li within ul
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    
+    var taskInfoEl = document.createElement("div"); //create div to hold task info
+    taskInfoEl.className = "task-info";
+    
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
