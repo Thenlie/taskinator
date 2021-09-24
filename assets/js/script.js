41fab5f50ba763e4383017e8b6fa422ae8cc4542
Leithen <leithen113@gmail.com>
2021-09-24 08:58:46 -0600

    Refractored code to have two functions instead of one

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,32 @@
 var formEl = document.querySelector("#task-form"); //assign variable to form element
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault(); //removes default browser behavior
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    }
+
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li"); //create new li within ul
     listItemEl.className = "task-item";
     
-    var taskInfoEl = document.createElement("div"); //create div to hold task info
+    var taskInfoEl = document.createElement("div"); //create div to hold task info within li
     taskInfoEl.className = "task-info";
     
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>"
 
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
-};
+}
 
-formEl.addEventListener("submit", createTaskHandler); //listen for form, submission
+formEl.addEventListener("submit", taskFormHandler); //listen for form, submission
 
